ddf5f7321fb9f081878aeb283d6000dfc35b1afa
M20233TN005 <20233tn005@utez.edu.mx>
2025-11-29 01:08:34 -0600

    Corrijo import de Bootstrap

--- racheros/app/__init__.py
+++ racheros/app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap
 
 # La variable principal se llama 'app'
 app = Flask(__name__)
 
-# --- CAMBIO CLAVE ---
-# La configuración ahora está directamente aquí para simplificar las importaciones
-# y evitar el error que estabas viendo.
-app.config['SECRET_KEY'] = 'llave-secreta-y-debe-ser-dificil-de-adivinar'
+# Configuración de la llave secreta
+app.config['SECRET_KEY'] = 'una-llave-secreta-muy-dificil-de-adivinar'
 
-bootstrap = Bootstrap5(app)
+# Inicialización de Bootstrap correcto
+bootstrap = Bootstrap(app)
 
 # Importamos las rutas al final para evitar importaciones circulares
 from app import routes
